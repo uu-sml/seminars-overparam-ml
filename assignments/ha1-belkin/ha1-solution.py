20908111e11e44dd3082f17bec9a858a21b4fd48
antonio <antonior92@gmail.com>
2021-10-08 09:21:01 +0200

    Add cond number and ridge option to ha1

--- assignments/ha1-belkin/ha1-solution.py
+++ assignments/ha1-belkin/ha1-solution.py
@@ -1,20 +1,29 @@
 import numpy as np
 from sklearn.kernel_approximation import RBFSampler
 from scipy.linalg import lstsq
+import scipy.linalg as linalg
 
 
 class RandomFourierFeatures(object):
-    def __init__(self, n_features: int = 20, gamma: float = 1.0, random_state: int = 0):
+    def __init__(self, n_features: int = 20, gamma: float = 1.0, random_state: int = 0, ridge: float = 0.0):
         self.gamma = gamma
         self.rbf_feature = RBFSampler(n_components=n_features, gamma=gamma, random_state=random_state)
         self.n_features = n_features
         self.random_state = random_state
         self.estim_param = None
+        self.ridge = ridge
 
     def fit(self, X, y):
         X = np.atleast_2d(X)
         Xf = self.rbf_feature.fit_transform(X)
-        self.estim_param, _resid, _rank, _s = lstsq(Xf, y)
+        if self.ridge <= 0:  # min norm solution
+            self.estim_param, _resid, _rank, _s = linalg.lstsq(Xf, y)
+        else:  # SVD implementation of ridge regression
+            u, s, vh = linalg.svd(Xf, full_matrices=False, compute_uv=True)
+            prod_aux = s / (
+                        self.ridge + s ** 2)  # If S = diag(s) => P = inv(S.T S + ridge * I) S.T => prod_aux = diag(P)
+            self.estim_param = (prod_aux * (y @ u)) @ vh  # here estim_param = V P U.T
+
         return self
 
     def predict(self, X):
@@ -26,6 +35,11 @@ class RandomFourierFeatures(object):
     def param_norm(self):
         return np.linalg.norm(self.estim_param)
 
+    def get_condition_number(self, X):
+        X = np.atleast_2d(X)
+        Xf = self.rbf_feature.transform(X)
+        s = np.linalg.svd(Xf, compute_uv=False)
+        return s[1] / s[-1]
 
 
 if __name__ == "__main__":
@@ -45,12 +59,13 @@ if __name__ == "__main__":
     mse_train = np.zeros(n_experiments)
     mse_test = np.zeros(n_experiments)
     param_norm = np.zeros(n_experiments)
+    conditining = np.zeros(n_experiments)
 
     # Repeat experiments for different number of parameters
     for i, fraction in enumerate(np.logspace(-1, 1, n_experiments)):
         # initialize and train model
         n_features[i] = int(fraction * n_train)
-        mdl = RandomFourierFeatures(n_features=n_features[i])
+        mdl = RandomFourierFeatures(n_features=n_features[i], gamma=0.001)
         mdl.fit(X_train, y_train)
         y_train_pred = mdl.predict(X_train)
 
@@ -61,6 +76,7 @@ if __name__ == "__main__":
         mse_train[i] = np.mean((y_train_pred - y_train)**2)
         mse_test[i] = np.mean((y_test_pred - y_test) ** 2)
         param_norm[i] = mdl.param_norm
+        conditining[i] = mdl.get_condition_number(X_train)
 
     fig, ax = plt.subplots(2)
     ax[0].plot(n_features / n_train, mse_train, label='train')
@@ -68,12 +84,12 @@ if __name__ == "__main__":
     ax[0].axvline(1, ls='--')
     ax[0].set_ylabel('Mean square error')
     ax[0].set_xscale('log')
-    ax[0].set_ylim((0, 5000))
+    ax[0].set_ylim((0, 1000))
     ax[0].legend()
-    ax[1].plot(n_features / n_train, param_norm)
+    ax[1].plot(n_features / n_train,  param_norm)
     ax[1].set_ylabel('Parameter norm')
     ax[1].set_xlabel('# features / # datapoints')
     ax[1].set_xscale('log')
     ax[1].axvline(1, ls='--')
-    ax[1].set_ylim((0, 5000))
+    ax[1].set_ylim((0, 2000))
     plt.show()
